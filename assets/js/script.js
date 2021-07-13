61b57fb01640e1d40889de8f17398367507a384c
FelipeGalarce <felipe.gp26@gmail.com>
2021-07-13 14:26:57 -0400

    se agregan comentarios y se modifica la funcion de calcular dias

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+/* ESTA FUNCION ES PARA QUE EL NUMERO INGRESADO SEA IGUAL O MAYOR A 1 QUE ES LO QUE SOLICITAN */
+
 function validar(val) {         
 
     if (val < 1) {
@@ -9,6 +11,7 @@ function validar(val) {
     }
     
 }
+/* ESTA ES LA FUNCION PARA REALIZAR TODAS LAS OPERACIONES BASICAS A 2 NUMEROS IGUALES O MAYORES A 1 */
 
 function activar1() {
     var num1 = validar(parseFloat(prompt("Escoge el primer numero. \n Debe ser Mayor a 0, de lo contrario se transformara a 1")));
@@ -23,10 +26,12 @@ function activar1() {
     alert("El Resultado de la Multiplicacion de " + num1 + " y " + num2 + " es igual a " + (multiplicacion));
     alert("El Resultado de la Division de " + num1 + " en " + num2 + " es igual a " + (division));
     alert("El Sobrante de la Division entre " + num1 + " y " + num2 + " es igual a " + (modal));
+    /* ESTO LO REPITO EN LAS DEMAS FUNCIONES, ES PARA MODIFICAR LA PANTALLA NEGRA QUE QUEDA BAJO LOS BOTONES CON EL RESULTADO */
     document.getElementById("pantalla").innerHTML = "<ul><li>El Resultado de la Suma de " + num1 + " y " + num2 + " es igual a " + (suma) + "</li><li> El Resultado de la Resta de " + num1 + " y " + num2 + " es igual a " + (resta) + "</li><li>El Resultado de la Multiplicacion de " + num1 + " y " + num2 + " es igual a " + (multiplicacion) + "</li><li> El Resultado de la Division de " + num1 + " en " + num2 + " es igual a " + (division) + "</li><li>El Sobrante de la Division entre " + num1 + " y " + num2 + " es igual a " + (modal) + "</li></ul>";
 
 }
 
+/* ESTA ES LA FUNCION PARA CALCULAR LOS GRADOS EN KELVIN Y FAHRENHEIT */
 
 function activar2() {
     var temp1 = parseFloat(prompt("Ingresa Una Temperatura en Grados Celsius"));
@@ -36,10 +41,10 @@ function activar2() {
 
 }
 
-
+/* DEFINO COMO CONSTANTES ESTOS DOS VALORES */
 const dias_en_semana = 7;
 const dias_anio = 365;
-
+/* ESTA ES LA FUNCION PARA CONVERTIR LOS DIAS, EN AÑOS, SEMANAS Y DIAS */
 function activar3() {
     var dias = parseInt(prompt("Ingresa Cuantos Dias Quieres Calcular"));
     var anios, semanas, dias_finales;
@@ -47,8 +52,9 @@ function activar3() {
     semanas = parseInt((dias % dias_anio) / dias_en_semana);
     dias_finales = (dias % 365) % dias_en_semana;
     alert("Sería(n) " + anios + " año(s), " + semanas + " semana(s) y " + dias_finales + " día(s)!!!")
-    document.getElementById("pantalla").innerHTML = "Sería(n) " + anios + " año(s), " + semanas + " semana(s) y " + dias_finales + " día(s)!!!";
+    document.getElementById("pantalla").innerHTML = "Los " + dias + " dias Ingresados, Sería(n) " + anios + " año(s), " + semanas + " semana(s) y " + dias_finales + " día(s)!!!";
 }
+/* ESTA ES LA FUNCION PARA SACAR EL PROMEDIO... EN LA ULTIMA PARTE PRACTIQUE LA INTERPOLACION */
 function activar4() {
     var valor1 = parseFloat(prompt("Ingresa el Primer Numero"))
     var valor2 = parseFloat(prompt("Ingresa el Segundo Numero"))
